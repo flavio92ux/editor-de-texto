900e5978ec4722205020bb7cc4e039944048833a
flavio.filho <flavio.filho@sada.com.br>
2022-04-10 19:34:06 -0300

    Usando do While na pratica

--- Program.cs
+++ Program.cs
@@ -23,7 +23,21 @@
 
     static void Abrir() {}
 
-    static void Editar() {}
+    static void Editar() {
+        Console.Clear();
+        Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+        Console.WriteLine("--------------------------");
+        string text = "";
+
+        do
+        {
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+        }
+        while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.Write(text);
+    }
 }
 
 Menu();
